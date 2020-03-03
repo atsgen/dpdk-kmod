4a42d176bd0c750bfb852bffcc4a51e17f6fa320
Prabhjot Singh Sethi <prabhjot@atsgen.com>
2020-03-03 12:50:21 +0000

    handle kmod install for centos
    Signed-off-by: Prabhjot Singh Sethi <prabhjot@atsgen.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,22 +5,32 @@ set -ex
 dir=$(dirname $0)
 source $dir/function
 
-if is_ubuntu; then
-  apt-get update
-  apt-get install -y make gcc dkms
-else
-  echo "UnSupported OS..."
-  exit 1
-fi
-
 sdir=$dir/../src
 version=0.1-dev
-templ=$(cat $sdir/dkms.conf.tmpl)
-content=$(eval "echo \"$templ\"")
-echo "$content" > $sdir/dkms.conf
-cp -r $sdir /usr/src/igb_uio-${version}
-dkms --verbose add -m igb_uio -v "${version}"
-dkms --verbose build -m igb_uio -v "${version}"
-dkms --verbose install -m igb_uio -v "${version}"
-depmod -a
 
+if is_ubuntu; then
+  templ=$(cat $sdir/dkms.conf.tmpl)
+  content=$(eval "echo \"$templ\"")
+  echo "$content" > $sdir/dkms.conf
+  cp -r $sdir /usr/src/igb_uio-${version}
+  apt-get update
+  apt-get install -y make gcc dkms
+  dkms --verbose add -m igb_uio -v "${version}"
+  dkms --verbose build -m igb_uio -v "${version}"
+  dkms --verbose install -m igb_uio -v "${version}"
+else
+  if is_centos; then
+    yum install -y gcc make
+    pushd $sdir
+      make
+    popd
+    d_dir=/lib/modules/`uname -r`/kernel/net/igb_uio
+    mkdir -p $d_dir
+    cp -f $sdir/igb_uio.ko $d_dir/
+  else
+    echo "UnSupported OS..."
+    exit 1
+  fi
+fi
+
+depmod -a
